3c1cf62737b0c981ba0bb96de8053d00ab51c8e1
kasper <kasperj2016@gmail.com>
2026-01-28 11:07:47 +0100

    Start

---
+++ src/main/java/com/example/touristguideapi1/TouristGuideApi1Application.java
@@ -0,0 +1,13 @@
+package com.example.touristguideapi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristGuideApi1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristGuideApi1Application.class, args);
+    }
+
+}
